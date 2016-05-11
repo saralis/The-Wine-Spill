b7a13b4e3cf92bbf709afc7bc0e388db693e52d6
Saralis Rivera <saralis.rivera@gmail.com>
2016-05-11 16:18:16 -0400

    Add answer model and answers migration

--- db/migrate/20160511151305_create_answers.rb
+++ db/migrate/20160511151305_create_answers.rb
@@ -2,8 +2,8 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
       t.string :body, null: false
-      t.references :user, foreign_key: true
-      t.references :question, foreign_key: true
+      t.references :user
+      t.references :question
 
       t.timestamps null: false
     end
