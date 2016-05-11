bcee2ffeecca41002601c3a8231453a28b33f952
David Walden <email": null>
2016-05-11 18:27:00 -0400

    Edit comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+post '/questions/:id/commments'
+end
+
+put '/questions/question_id/comments/:id'
+end
+
+delete '/questions/question_id/comments/:id'
+end
+
+post '/answers/:id/comments'
+end
+
+put '/answers/answer_id/comments/:id'
+end
+
+delete '/answers/answer_id/comments/:id'
+end
