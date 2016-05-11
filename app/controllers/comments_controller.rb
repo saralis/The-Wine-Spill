7d040110c92610319dac2e91d18cb82088674604
David Walden <email": null>
2016-05-11 19:04:33 -0400

    Work on comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,63 @@
-post '/questions/:id/commments'
-end
+post '/questions/:question_id/comments' do
+  @question = Question.find(params[:question_id])
 
-put '/questions/question_id/comments/:id'
-end
+  @comment = @question.comments.new(params[:comment])
 
-delete '/questions/question_id/comments/:id'
+  @comment.save
+  redirect "/questions/#{@question.id}"
 end
+# this will cause view count to increment
 
-post '/answers/:id/comments'
-end
+put '/questions/:question_id/comments/:id' do
+  @question = Question.find(params[:question_id])
 
-put '/answers/answer_id/comments/:id'
-end
+  @comment = @question.comments.find(params[:id])
 
-delete '/answers/answer_id/comments/:id'
+  @comment.update_attributes(params[:comment])
+  redirect "/questions/#{@question.id}"
 end
+# this will cause view count to increment
+
+delete '/questions/:question_id/comments/:id' do
+  @question = Question.find(params[:question_id])
+
+  @comment = @question.comments.find(params[:id])
+
+  @comment.destroy
+
+  redirect "/questions/#{@question.id}"
+end
+# this will cause view count to increment
+
+
+
+post '/answers/:answer_id/comments' do
+  @answer = Answer.find(params[:answer_id])
+
+  @comment = @answer.comments.new(params[:comment])
+
+  @comment.save
+  redirect "/answers/#{@answer.id}"
+end
+# this will cause view count to increment
+
+put '/answers/:answer_id/comments/:id' do
+  @answer = Answer.find(params[:answer_id])
+
+  @comment = @answer.comments.find(params[:id])
+
+  @comment.update_attributes(params[:comment])
+  redirect "/answers/#{@answer.id}"
+end
+# this will cause view count to increment
+
+delete '/answers/:answer_id/comments/:id' do
+  @answer = Answer.find(params[:answer_id])
+
+  @comment = @answer.comments.find(params[:id])
+
+  @comment.destroy
+
+  redirect "/answers/#{@answer.id}"
+end
+# this will cause view count to increment
