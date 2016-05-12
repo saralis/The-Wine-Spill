c81dc30d42ee817a646b7e8566d0b41f190df025
David Walden <email": null>
2016-05-12 18:02:25 -0400

    edit comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -30,10 +30,10 @@ end
 post '/answers/:answer_id/comments' do
   require_user
   @answer = Answer.find(params[:answer_id])
-  params[:comment][:answer_id] = current_user.id
+  params[:comment][:user_id] = current_user.id
   @comment = @answer.comments.new(params[:comment])
   @comment.save
-  redirect "/questions/#{@question.id}"
+  redirect "/questions/#{@answer.question.id}"
 end
 
 # Edit answer
