6301f035a84dc0f164f08854d620e68362cfd5f9
Ayaz Uddin <ayaz2589@gmail.com>
2016-05-12 10:07:50 -0400

    update votes migration

--- db/migrate/20160511152627_create_votes.rb
+++ db/migrate/20160511152627_create_votes.rb
@@ -2,7 +2,7 @@ class CreateVotes < ActiveRecord::Migration
   def change
   	create_table :votes do |t|
   		t.integer :count
-  		t.references :users, null: false
+  		t.references :user, null: false
   		t.references :votable, polymorphic: true, index: true
 
   		t.timestamps null: false
