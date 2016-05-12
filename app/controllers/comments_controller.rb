3a602dc21151d1026e6cfd5ede486c9c446c3875
David Walden <email": null>
2016-05-12 10:48:23 -0400

    Add comments to questions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,21 @@
+# Questions
 post '/questions/:question_id/comments' do
+  require_user
   @question = Question.find(params[:question_id])
+  params[:comment][:user_id] = current_user.id
   @comment = @question.comments.new(params[:comment])
   @comment.save
   redirect "/questions/#{@question.id}"
 end
 
-put '/questions/:question_id/comments/:id' do
-  require_same_user(current_user)
-  @question = Question.find(params[:question_id])
-  @comment = @question.comments.find(params[:id])
-  @comment.update_attributes(params[:comment])
-  redirect "/questions/#{@question.id}"
-end
-
+# Edit question
+# put '/questions/:question_id/comments/:id' do
+#   require_same_user(current_user)
+#   @question = Question.find(params[:question_id])
+#   @comment = @question.comments.find(params[:id])
+#   @comment.update_attributes(params[:comment])
+#   redirect "/questions/#{@question.id}"
+# end
 
 delete '/questions/:question_id/comments/:id' do
   require_same_user(current_user)
@@ -22,6 +25,8 @@ delete '/questions/:question_id/comments/:id' do
   redirect "/questions/#{@question.id}"
 end
 
+
+# Answers
 post '/answers/:answer_id/comments' do
   @answer = Answer.find(params[:answer_id])
   @comment = @answer.comments.new(params[:comment])
@@ -29,6 +34,7 @@ post '/answers/:answer_id/comments' do
   redirect "/answers/#{@answer.id}"
 end
 
+# Edit answer
 put '/answers/:answer_id/comments/:id' do
   require_same_user(current_user)
   @answer = Answer.find(params[:answer_id])
