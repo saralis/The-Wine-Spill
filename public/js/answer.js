55dcca7c1f0cde45510e691e6eb76d4cc6a8b7f7
Saralis Rivera <saralis.rivera@gmail.com>
2016-05-12 20:52:53 -0400

    Answers ajax not yet complete

--- public/js/answer.js
+++ public/js/answer.js
@@ -2,10 +2,14 @@
 //   $("#answer-form").click(function(e) {
 //     e.preventDefault();
 
+//     var data = $(e.target).serialize();
+//     var url = e.target.action;
+//     var type = e.target.method;
+
 //     var ajaxRequest= $.ajax({
-//       data: $(e.target).serialize(),
-//       type: e.target.method,
-//       url: e.target.action,
+//       data: data,
+//       type: type,
+//       url: url,
 //     });
 
 //     ajaxRequest.done(function(response){
